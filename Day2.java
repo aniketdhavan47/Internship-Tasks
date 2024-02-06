4968f74358e70cda9abefbab34355099d28f7706
Aniket Dhavan <81401569+aniketdhavan47@users.noreply.github.com>
2024-02-06 15:09:46 +0530

    Functionality For Single Bus Updated (Day-2)
    Functionalities added without any specific route 
    Added Functionalities-
    1)CheckAvailability
    2)bookBus
    3)cancelBooking
    4)showBookingDetails

--- Day2.java
+++ Day2.java
@@ -1,74 +1,91 @@
-package com.backtracking;
-import java.time.LocalDate;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import java.time.format.DateTimeParseException;
+import java.util.*;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
- class Bus {
-    private int busNumber;
-    private int numberOfSeats;
-    private int fare;
-    private LocalDate bookedDate;
+// Bus will have property like bus number, number of seats and fare
+class Bus {
+    int busNumber;
+    int totalSeats;
+    int remainingSeats;
+    int fare;
+    public static final java.time.format.DateTimeFormatter DATE_FORMAT = java.time.format.DateTimeFormatter
+            .ofPattern("dd-MM-yyyy");
+    ArrayList<HashMap<LocalDate, Integer>> bookingDetails;
 
-    public Bus(int busNumber, int numberOfSeats, int fare) {
-        this.busNumber = busNumber;
-        this.numberOfSeats = numberOfSeats;
+    Bus(int bNo, int total, int fare) {
+        this.busNumber = bNo;
+        this.totalSeats = total;
         this.fare = fare;
+        this.remainingSeats = total;
     }
 
     public int getBusNumber() {
-        return busNumber;
+        return this.busNumber;
     }
 
-    public int getNumberOfSeats() {
-        return numberOfSeats;
+    public int totalSeats() {
+        return this.totalSeats;
+    }
+
+    public void setRemainingSeats(int num) {
+        this.remainingSeats = num;
+    }
+
+    public int getRemainingSeats() {
+        return this.remainingSeats;
     }
 
     public int getFare() {
-        return fare;
+        return this.fare;
     }
 
-    public boolean isBooked(LocalDate date) {
-        return bookedDate != null && bookedDate.equals(date);
+    // Functionalities
+    public boolean isBooked(LocalDate d1) {
+        for (int i = 0; i < bookingDetails.size(); i++) {
+            if (bookingDetails.get(i).get(d1) == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void book(LocalDate date, int numberOfSeats) {
-        this.bookedDate = date;
-        this.numberOfSeats -= numberOfSeats;
-    }
-
-    public void cancelBooking() {
-        this.bookedDate = null;
-    }
-
-    public static final java.time.format.DateTimeFormatter DATE_FORMAT =
-            java.time.format.DateTimeFormatter.ofPattern("dd-MM-yyyy");
 }
 
- class Booking {
-    private Bus bus;
-    private LocalDate date;
-    private int numberOfSeats;
-    private int bookingId;
+class Booking {
+    Bus bus;
+    String source;
+    String destination;
+    LocalDate date;
+    int numberOfSeats;
+    String customerName;
+    long contactDetails;
+    int bookingId;
 
-    public Booking(Bus bus, LocalDate date, int numberOfSeats) {
-        this.bus = bus;
-        this.date = date;
-        this.numberOfSeats = numberOfSeats;
-        this.bookingId = bus.getBusNumber() * 1000 + bus.getNumberOfSeats() * 100 + numberOfSeats;
-    }
-
-    public int getBookingId() {
-        return bookingId;
+    // b1,source,destination,date,seats,customerName,contactNumber
+    Booking(Bus b1, String source, String destination, LocalDate d1, int seats, String name, long contact) {
+        this.bus = b1;
+        this.source = source;
+        this.destination = destination;
+        this.date = d1;
+        this.numberOfSeats = seats;
+        this.customerName = name;
+        this.contactDetails = contact;
+        this.bookingId = (int) Math.round(Math.random() * 1000);
+        b1.setRemainingSeats(b1.getRemainingSeats() - seats);
+        System.out.println("Booking Successful");
     }
 
     public Bus getBus() {
         return bus;
+    }
+
+    public String getSource() {
+        return this.source;
+    }
+
+    public String getDestination() {
+        return this.destination;
     }
 
     public LocalDate getDate() {
@@ -76,28 +93,29 @@ import java.util.List;
     }
 
     public int getNumberOfSeats() {
-        return numberOfSeats;
+        return this.numberOfSeats;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public long getContactDetails() {
+        return this.contactDetails;
+    }
+
+    public int getBookingId() {
+        return this.bookingId;
     }
 }
-	
 
-public class BusBookingSystem {
-    private static List<Bus> buses;
-    private static List<Booking> bookings;
-
+public class BusBooking {
     public static void main(String[] args) {
-        buses = new ArrayList<>();
-        bookings = new ArrayList<>();
 
-        // Add buses
-        Bus bus1 = new Bus(1, 50, 100);
-        
-        
-        buses.add(bus1);
-       
-
-        // Main menu
+        // Main Menu
+        Bus b1 = new Bus(1001, 50, 100);
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Booking> details = new ArrayList<>();
         int choice;
         do {
             System.out.println("1. Check availability");
@@ -109,16 +127,36 @@ public class BusBookingSystem {
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    checkAvailability();
+                    boolean isFull = checkAvailability(b1);
+                    if (isFull) {
+                        System.out.println("------------------------------");
+                        System.out.println("No Seats Are Avilable");
+                        System.out.println("------------------------------");
+                    } else {
+                        System.out.println("------------------------------");
+                        System.out.println(b1.getRemainingSeats() + " seats Are Avilable");
+                        System.out.println("------------------------------");
+                    }
+
                     break;
                 case 2:
-                    bookBus();
+                    Booking book1 = bookBus(b1);
+                    if (book1 == null) {
+                        System.out.println("------------------------------");
+                        System.out.println("Can not Book Seats");
+                        System.out.println("------------------------------");
+                    } else {
+                        System.out.println("------------------------------");
+                        System.out.println("Your Booking id is :" + book1.bookingId);
+                        System.out.println("------------------------------");
+                    }
+                    details.add(book1);
                     break;
                 case 3:
-//                    cancelBooking();
+                    cancelBooking(details);
                     break;
                 case 4:
-//                    printBookingBill();
+                    printBookingBill(details);
                     break;
                 case 5:
                     System.out.println("Exiting...");
@@ -128,60 +166,128 @@ public class BusBookingSystem {
             }
         } while (choice != 5);
     }
+    // Functionality
 
-    private static void checkAvailability() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter date (dd-mm-yyyy): ");
-        String dateInput = scanner.next();
-        LocalDate date = LocalDate.parse(dateInput, Bus.DATE_FORMAT);
-
-        boolean isAvailable = true;
-        for (Bus bus : buses) {
-            if (bus.isBooked(date)) {
-                isAvailable = false;
-                break;
-            }
-        }
-
-        if (isAvailable) {
-            System.out.println("Bus is available for the given date.");
-        } else {
-            System.out.println("Bus is not available for the given date.");
-        }
+    public static boolean checkAvailability(Bus b1) {
+        if (b1.getRemainingSeats() == 0)
+            return true;
+        return false;
     }
 
-    private static void bookBus() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter date (dd-MM-yyyy): ");
-        String dateInput = scanner.next();
-        LocalDate date = LocalDate.parse(dateInput, Bus.DATE_FORMAT);
+    public static Booking bookBus(Bus b1) {
+        Booking book1 = null;
+        try{
 
-        boolean isBooked = false;
-        for (Bus bus : buses) {
-            if (!bus.isBooked(date)) {
-                System.out.println("Bus number: " + bus.getBusNumber());
-                System.out.println("Number of seats: " + bus.getNumberOfSeats());
-                System.out.println("Fare: " + bus.getFare());
-                
-                System.out.print("Enter number of seats to book: ");
-                int numberOfSeats = scanner.nextInt();
+            System.out.println("Enter the Source Stop :");
+            Scanner sc = new Scanner(System.in);
+            String source = sc.next();
+            System.out.println("Enter the Destination Stop:");
+            String destination = sc.next();
+            System.out.println("Enter the Date in Format (dd-mm-yyyy):");
+            String userInput = sc.next();
+            LocalDate date = null;
 
-                if (numberOfSeats <= bus.getNumberOfSeats()) {
-                    bus.book(date, numberOfSeats);
-                    Booking booking = new Booking(bus, date, numberOfSeats);
-                    bookings.add(booking);
-                    System.out.println("Bus booked successfully.");
-                    isBooked = true;
-                    break;
-                } else {
-                    System.out.println("Invalid number of seats. Please try again.");
+            date = LocalDate.parse(userInput, b1.DATE_FORMAT);
+
+            // Validating Booking Date
+            LocalDate currDate = LocalDate.now();
+                if (date.isBefore(currDate)) {
+                    System.out.println("------------------------------");
+                    System.out.println("Invalid Date-Can Not Book Seats For Previous Dates");
+                    System.out.println("------------------------------");
+                    return null;
+                }
+
+            System.out.println("Enter the number of seats want to book");
+            int seats = sc.nextInt();
+            System.out.println("Enter Customer Name:");
+            String customerName = sc.next();
+            System.out.println("Enter Contact Number");
+            long contactNumber = sc.nextLong();
+            
+                if (b1.getRemainingSeats() >= seats) {
+                    book1 = new Booking(b1, source, destination, date, seats, customerName, contactNumber);
+                }
+        
+        }catch(InputMismatchException ime){
+            System.out.println("------------------------------");
+            System.out.println("Input MisMatch");
+            System.out.println("------------------------------");
+        }catch(DateTimeParseException dtpe){
+            System.out.println("------------------------------");
+            System.out.println("Invald Date Format");
+            System.out.println("------------------------------");
+        }
+      
+        return book1;
+    }
+
+    public static void cancelBooking(ArrayList<Booking> details) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Booking Id :");
+        int id = sc.nextInt();
+        boolean isFind = false;
+        if (details.size() > 0) {
+            for (int i = 0; i < details.size(); i++) {
+                try {
+                    if (id == details.get(i).getBookingId()) {
+                        details.remove(i);
+                        isFind = true;
+                        System.out.println("Booking Cancelled");
+                    }
+                } catch (NullPointerException npe) {
+                    System.out.println("Null");
                 }
             }
         }
+        if (!isFind) {
+            System.out.println("Booking is Not Avilable");
+        }
 
-        if (!isBooked) {
-            System.out.println("Bus is not available for the given date.");
+    }
+
+    // ---------------------
+    // Agency name: xyz travels, Date:
+    // Bus number:
+    // Booked seats:
+    // Source stop:pune
+    // destination:mumbai
+    // ------------------------
+    // Customer name: Contact:
+    // ------------------------
+    // Total Fare:
+    // -----------------------
+    public static void printBookingBill(ArrayList<Booking> details) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Booking Id");
+        int id = sc.nextInt();
+        boolean isFind = false;
+        if (details.size() > 0) {
+            for (int i = 0; i < details.size(); i++) {
+                try {
+                    if (id == details.get(i).getBookingId()) {
+                        Booking book1 = details.get(i);
+                        System.out.println("-----------------Booking Details------------");
+                        System.out.println("Bus Number :" + book1.getBus().getBusNumber());
+                        System.out.println("Booked Seats :" + book1.getNumberOfSeats());
+                        System.out.println("Source Stop :" + book1.getSource());
+                        System.out.println("Destination Stop :" + book1.getDestination());
+                        System.out.println("---------------------------------");
+                        System.out.println(
+                                "Customer Name :" + book1.getCustomerName() + " Contact:" + book1.getContactDetails());
+                        System.out.println("-----------------------------------------------");
+                        isFind = true;
+                    }
+                } catch (NullPointerException npe) {
+                   System.out.println("Null");
+                }
+            }
+        }
+        if (isFind == false) {
+            System.out.println("---------------------------------");
+            System.out.println("Booking Not Avilable");
+            System.out.println("---------------------------------");
         }
     }
+
 }
-    
